c54df3f4f85989ad9e1db9078cb4f214947da37b
ZTL-UwU <zhangtianli2006@163.com>
2024-05-26 15:59:10 +0800

    chore: init
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ app.config.ts
@@ -0,0 +1,90 @@
+export default defineAppConfig({
+  shadcnDocs: () => ({
+    site: {
+      name: 'shadcn-docs-nuxt'
+    },
+    header: {
+      title: 'shadcn-docs-starter',
+      showTitle: true,
+      darkModeToggle: true,
+      logo: {
+        light: '/logo.svg',
+        dark: '/logo-dark.svg',
+      },
+      nav: [],
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }],
+    },
+    aside: {
+      useLevel: true,
+      collapse: false,
+    },
+    main: {
+      breadCrumb: true,
+      showTitle: true,
+      codeIcon: defaultCodeIcon,
+    },
+    footer: {
+      credits: 'Copyright © 2024',
+      links: [{
+        icon: 'lucide:github',
+        to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
+        target: '_blank',
+      }],
+    },
+    toc: {
+      enable: true,
+      title: 'On This Page',
+    },
+    search: {
+      enable: true,
+      inAside: false,
+    }
+  })
+});
+
+const defaultCodeIcon = {
+  'package.json': 'vscode-icons:file-type-node',
+  'tsconfig.json': 'vscode-icons:file-type-tsconfig',
+  '.npmrc': 'vscode-icons:file-type-npm',
+  '.editorconfig': 'vscode-icons:file-type-editorconfig',
+  '.eslintrc': 'vscode-icons:file-type-eslint',
+  '.eslintrc.cjs': 'vscode-icons:file-type-eslint',
+  '.eslintignore': 'vscode-icons:file-type-eslint',
+  'eslint.config.js': 'vscode-icons:file-type-eslint',
+  'eslint.config.mjs': 'vscode-icons:file-type-eslint',
+  'eslint.config.cjs': 'vscode-icons:file-type-eslint',
+  '.gitignore': 'vscode-icons:file-type-git',
+  'yarn.lock': 'vscode-icons:file-type-yarn',
+  '.env': 'vscode-icons:file-type-dotenv',
+  '.env.example': 'vscode-icons:file-type-dotenv',
+  '.vscode/settings.json': 'vscode-icons:file-type-vscode',
+  'nuxt': 'vscode-icons:file-type-nuxt',
+  '.nuxtrc': 'vscode-icons:file-type-nuxt',
+  '.nuxtignore': 'vscode-icons:file-type-nuxt',
+  'nuxt.config.js': 'vscode-icons:file-type-nuxt',
+  'nuxt.config.ts': 'vscode-icons:file-type-nuxt',
+  'nuxt.schema.ts': 'vscode-icons:file-type-nuxt',
+  'tailwind.config.js': 'vscode-icons:file-type-tailwind',
+  'tailwind.config.ts': 'vscode-icons:file-type-tailwind',
+  'vue': 'vscode-icons:file-type-vue',
+  'ts': 'vscode-icons:file-type-typescript',
+  'tsx': 'vscode-icons:file-type-typescript',
+  'mjs': 'vscode-icons:file-type-js',
+  'cjs': 'vscode-icons:file-type-js',
+  'js': 'vscode-icons:file-type-js',
+  'jsx': 'vscode-icons:file-type-js',
+  'md': 'vscode-icons:file-type-markdown',
+  'py': 'vscode-icons:file-type-python',
+  'npm': 'vscode-icons:file-type-npm',
+  'pnpm': 'vscode-icons:file-type-pnpm',
+  'npx': 'vscode-icons:file-type-npm',
+  'yarn': 'vscode-icons:file-type-yarn',
+  'bun': 'vscode-icons:file-type-bun',
+  'yml': 'vscode-icons:file-type-yaml',
+  'json': 'vscode-icons:file-type-json',
+  'terminal': 'lucide:terminal',
+};
---
+++ content/index.md
@@ -0,0 +1,26 @@
+---
+title: Home
+navigation: false
+---
+
+::hero
+---
+announcement:
+  title: 'Release v1.0.0'
+  icon: 'noto:party-popper'
+  to: /getting-started
+actions:
+  - name: Get Started
+    to: /getting-started
+  - name: GitHub
+    variant: outline
+    to: https://github.com/ZTL-UwU/shadcn-docs-nuxt-starter
+    leftIcon: 'lucide:github'
+---
+
+#title
+Effortless and Beautiful<br>Docs Template.
+
+#description
+Beautifully designed Nuxt Content template with shadcn-vue.<br>Customizable. Compatible. Open Source.
+::
