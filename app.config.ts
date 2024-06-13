1c4f940e9eba2293ed5458c52540bbf133857403
ZTL-UwU <zhangtianli2006@163.com>
2024-06-13 18:11:21 +0800

    feat: update config
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,8 @@
 export default defineAppConfig({
-  shadcnDocs: () => ({
+  shadcnDocs: {
     site: {
-      name: 'shadcn-docs-nuxt'
+      name: 'shadcn-docs-nuxt',
+      description: 'Beautifully designed Nuxt Content template built with shadcn-vue. Customizable. Compatible. Open Source.',
     },
     header: {
       title: 'shadcn-docs-starter',
@@ -25,7 +26,6 @@ export default defineAppConfig({
     main: {
       breadCrumb: true,
       showTitle: true,
-      codeIcon: defaultCodeIcon,
     },
     footer: {
       credits: 'Copyright © 2024',
@@ -54,49 +54,5 @@ export default defineAppConfig({
       enable: true,
       inAside: false,
     }
-  })
+  }
 });
-
-const defaultCodeIcon = {
-  'package.json': 'vscode-icons:file-type-node',
-  'tsconfig.json': 'vscode-icons:file-type-tsconfig',
-  '.npmrc': 'vscode-icons:file-type-npm',
-  '.editorconfig': 'vscode-icons:file-type-editorconfig',
-  '.eslintrc': 'vscode-icons:file-type-eslint',
-  '.eslintrc.cjs': 'vscode-icons:file-type-eslint',
-  '.eslintignore': 'vscode-icons:file-type-eslint',
-  'eslint.config.js': 'vscode-icons:file-type-eslint',
-  'eslint.config.mjs': 'vscode-icons:file-type-eslint',
-  'eslint.config.cjs': 'vscode-icons:file-type-eslint',
-  '.gitignore': 'vscode-icons:file-type-git',
-  'yarn.lock': 'vscode-icons:file-type-yarn',
-  '.env': 'vscode-icons:file-type-dotenv',
-  '.env.example': 'vscode-icons:file-type-dotenv',
-  '.vscode/settings.json': 'vscode-icons:file-type-vscode',
-  'nuxt': 'vscode-icons:file-type-nuxt',
-  '.nuxtrc': 'vscode-icons:file-type-nuxt',
-  '.nuxtignore': 'vscode-icons:file-type-nuxt',
-  'nuxt.config.js': 'vscode-icons:file-type-nuxt',
-  'nuxt.config.ts': 'vscode-icons:file-type-nuxt',
-  'nuxt.schema.ts': 'vscode-icons:file-type-nuxt',
-  'tailwind.config.js': 'vscode-icons:file-type-tailwind',
-  'tailwind.config.ts': 'vscode-icons:file-type-tailwind',
-  'vue': 'vscode-icons:file-type-vue',
-  'ts': 'vscode-icons:file-type-typescript',
-  'tsx': 'vscode-icons:file-type-typescript',
-  'mjs': 'vscode-icons:file-type-js',
-  'cjs': 'vscode-icons:file-type-js',
-  'js': 'vscode-icons:file-type-js',
-  'jsx': 'vscode-icons:file-type-js',
-  'md': 'vscode-icons:file-type-markdown',
-  'mdc': 'vscode-icons:file-type-markdown',
-  'py': 'vscode-icons:file-type-python',
-  'npm': 'vscode-icons:file-type-npm',
-  'pnpm': 'vscode-icons:file-type-pnpm',
-  'npx': 'vscode-icons:file-type-npm',
-  'yarn': 'vscode-icons:file-type-yarn',
-  'bun': 'vscode-icons:file-type-bun',
-  'yml': 'vscode-icons:file-type-yaml',
-  'json': 'vscode-icons:file-type-json',
-  'terminal': 'lucide:terminal',
-};
